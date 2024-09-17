2eed6d1a186f868397d52a696c15b6a30814b5b9
benardp <pierre.benard@laposte.net>
2024-09-17 13:17:13 +0200

    Fix unregister error in __init__.py

--- __init__.py
+++ __init__.py
@@ -24,15 +24,16 @@ def register():
 
 def unregister():
 	smear_control_panel.unregister()
-	rootNodeTree = bpy.data.node_groups['Smear Frames Controler']
-	nodesToDelete = ['Smear Frames Controler']
-	for node in bpy.data.node_groups:
-		if rootNodeTree.contains_tree(node):
-			nodesToDelete.append(node.name)
-
-	for node in bpy.data.node_groups:
-		if sum([node.name.startswith(n) for n in nodesToDelete]) > 0:
-			bpy.data.node_groups.remove(node)
+	if 'Smear Frames Controler' in bpy.data.node_groups:
+		rootNodeTree = bpy.data.node_groups['Smear Frames Controler']
+		nodesToDelete = ['Smear Frames Controler']
+		for node in bpy.data.node_groups:
+			if rootNodeTree.contains_tree(node):
+				nodesToDelete.append(node.name)
+	
+		for node in bpy.data.node_groups:
+			if sum([node.name.startswith(n) for n in nodesToDelete]) > 0:
+				bpy.data.node_groups.remove(node)
 
 if __name__ == "__main__":
 	register()
